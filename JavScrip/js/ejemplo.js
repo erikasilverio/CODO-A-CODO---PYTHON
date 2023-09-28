9c60c338c4306efe01c9f71956284cee190202d2
Erika Silverio <cms.silverio@gmail.com>
2023-09-27 22:07:07 -0300

    ejemplos JS

--- JavScrip/js/ejemplo.js
+++ JavScrip/js/ejemplo.js
@@ -35,7 +35,6 @@ console.log(miArray[1])
 var miArray = [[32,64,128], [10,20,30], [[2,3,5,7,11,13], [2,4,6,8]]] // comeca com 0
 console.log(miArray[2][0][5])
 
-*/
 
 
 let num1, num2, suma
@@ -44,3 +43,63 @@ num2 = parseInt(prompt("Ingrese otro numero"))
 
 suma = num1 + num2
 console.log('la suma es' , suma)
+*/
+
+
+
+
+}
+
+/*hacer um dicionario de 10 palabras
+gato - cat
+casa - house
+caballo - hourse
+
+
+let valor = parseInt(prompt("Ingrese en numero: \n"))
+switch (valor) {
+    case 1: document.write('One')
+        
+    break;
+    case 2: document.write('Two')
+    
+    break;
+    case 3: document.write('Tree')
+    
+    break;
+    case 4: document.write('Four')
+    
+    break;
+    case 5: document.write('Five')
+        
+       
+
+
+    default:document.write('No es un valor')
+        break;
+ */
+
+
+
+        //WHILE (BUCLE, GRANDE QUANTIDADE DE VEZES QUE SE EXECUTA AS INSTRUCOES)
+
+       // let x = 1
+       // while(x <= 100){
+       //     document.write(x)
+       //     document.write('<br>')
+       //     x++
+       //     
+       // }
+
+       // 11 - 22 - 23 - 44 <<10 veces >>
+
+       let serie = 11
+       let i = 1
+       while ( i <= 10 ) {
+
+        document.write(serie)
+        document.write('<br>')
+        i++
+        serie += 11
+        
+       }
